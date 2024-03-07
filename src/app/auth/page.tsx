519354a00edf89c15d8e55f90422ad908f24e68f
l1nhx <linhhandsom@gmail.com>
2024-03-07 11:48:58 +0700

    refactor(auth page): change from custom to auth form

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,36 +1,74 @@
 'use client'
-import { Card, Typography } from 'antd'
-import React from 'react'
+import { Card, Checkbox, Form, FormProps, Typography } from 'antd'
+import React, { useEffect } from 'react'
 import { UserOutlined } from '@ant-design/icons';
 import { Button, Input, Space } from 'antd';
 import { EyeInvisibleOutlined, EyeTwoTone, LockOutlined } from '@ant-design/icons'
 import { useRouter } from 'next/navigation';
+type FieldType = {
+  username?: string;
+  password?: string;
+  remember?: string;
+};
 
+const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
+  console.log('Success:', values);
+};
+
+const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
+  console.log('Failed:', errorInfo);
+};
 const Login = () => {
   const router = useRouter()
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  const [passwordVisible, setPasswordVisible] = React.useState(false);
-  const checkLogin = ()=>{
+
+  const checkLogin = () => {
     router.push('/dashboard')
   }
+  
   return (
     <div className='flex justify-center text-center'>
+      <Card className='mt-8 items-center'>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item<FieldType>
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Card className='mt-8'>
-        <div className='grid'>
-          <Typography style={{ margin: '10px 0' }}>Admin? Please log in to view content</Typography>
-          <Input size="large" placeholder="Username" prefix={<UserOutlined />} style={{ margin: '10px 0' }} />
-          <Input.Password
-            size='large'
-            placeholder="Password"
-            visibilityToggle={{ visible: passwordVisible, onVisibleChange: setPasswordVisible }}
-            prefix={<LockOutlined />}
-            style={{ margin: '10px 0' }}
-          />
-          <Button style={{ margin: '10px 0' }} onClick={checkLogin}>
-            Log in
+        <Form.Item<FieldType>
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{ offset: 8, span: 16 }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit" onClick={checkLogin} style={{background:'#407fff'}}>
+            Submit
           </Button>
-        </div>
+        </Form.Item>
+      </Form>
       </Card>
     </div>
   )
